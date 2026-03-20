62c112399af4a16fe19de0248f1e7b42e79c706e
cesarrsegovia <cesarr.segovia27@gmail.com>
2026-03-19 22:26:09 -0300

    correcciones

--- src/routes/users.js
+++ src/routes/users.js
@@ -33,4 +33,4 @@ router.post('/login', guestMiddleware, loginValidations, usersController.process
 router.post('/register', guestMiddleware, registerValidations, usersController.processRegister);
 router.get('/logout', usersController.logout);
 
-module.exports = router;//adsasd
+module.exports = router;
